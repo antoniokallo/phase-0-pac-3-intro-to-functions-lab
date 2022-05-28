7d79d60ac46edd252dc16a808ed2577a2746681e
antoniokallo <antoniokallo@gmail.com>
2022-05-28 16:51:52 -0400

    commit tree 752b125bd2b9f3e30305a61538a1813b4851483e

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string){
+      return string.toLowerCase();
+  }
+  function logShout(string){
+console.log("HELLO");
+;
+  }
+  function logWhisper(string){
+      console.log("hello");
+  }
+
+function sayHiToHeadphonedRoommate(greeting){
+     
+
+if (greeting === greeting.toUpperCase()) {
+  return "YES INDEED!";
+}
+ if (greeting === greeting.toLowerCase()) {
+  return "I can't hear you!"
+}
+if (greeting === "Let's have dinner together!")
+return "I would love to!"
+}
+
